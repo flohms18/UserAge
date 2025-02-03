399ef853989b137c8de1000784450b2f6adabf6d
flotr18 <trouilletflorent@outlook.com>
2025-02-03 19:23:07 +0100

    Fixed Space Issued

--- UserAge.py
+++ UserAge.py
@@ -1,9 +1,9 @@
 def UserAge():
     age = int(input("Enter your age: "))
     if age < 18:
-        print("You are" + str(age) + "you are a minor.")
+        print("You are" + str(age) + ", you are a minor.")
         
     else:
-        print("You are" + str(age) + "you are a major.")
+        print("You are" + str(age) + ", you are a major.")
 
 UserAge()
